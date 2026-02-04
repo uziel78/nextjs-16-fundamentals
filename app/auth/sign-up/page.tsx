ea6055ad08c8fde12870addb6d6ed3c8a2ed8b1d
uziel78 <rune.husa78@gmail.com>
2026-02-04 13:28:28 +0100

    navbar moved to shared layout, to not show it in auth-routes

---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1>Sign up page</h1>
+    </div>
+  );
+}
--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import { ThemeToggle } from './theme-toggle';
 
 export function Navbar() {
