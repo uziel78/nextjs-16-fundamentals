5d6bb79249e1fd0f8b0a22b4fbc3abaa49d87148
uziel78 <rune.husa78@gmail.com>
2026-02-06 18:02:22 +0100

    auth form in progress

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,57 @@
+'use client';
+
+import { signUpSchema } from '@/app/schemas/auth';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import {
+  FieldError,
+  FieldGroup,
+  Field,
+  FieldLabel,
+} from '@/components/ui/field';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+
 export default function SignUpPage() {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
   return (
-    <div>
-      <h1>Sign up page</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Controller
+              name='name'
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input placeholder='John Doe' {...field} />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
