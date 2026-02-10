60f4da7ed0449d0b48f266cfecfa9fa0c78a163d
uziel78 <rune.husa78@gmail.com>
2026-02-10 10:52:37 +0100

    convex server setup & working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ConvexClientProvider } from '@/components/web/ConvexClientProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,7 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className='max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8'>
-            {children}
+            <ConvexClientProvider>{children}</ConvexClientProvider>
           </main>
         </ThemeProvider>
       </body>
