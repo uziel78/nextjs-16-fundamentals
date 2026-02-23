951753f0da794b607fd9ed8bf503fe7d7f427e5a
uziel78 <rune.husa78@gmail.com>
2026-02-23 11:24:31 +0100

    feat: refactor BlogPage to use Suspense for loading state and add Skeleton component for improved UX

--- app/(shared-layout)/blog/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingBlog() {
-  return (
-    <h1 className='text-5xl font-bold text-center text-primary p-10'>
-      Loading...
-    </h1>
-  );
-}
--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,12 +4,11 @@ import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 import { fetchQuery } from 'convex/nextjs';
+import { Suspense } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
 
-// This is a Server Component that fetches data from the Convex backend and renders it on the server.
-// No automatic convex data updates will occur in this component (using fetchQuery), but it can fetch data directly from the database without needing to define an API route.
-export default async function BlogPage() {
-  const data = await fetchQuery(api.posts.getPosts);
-
+// This is the main blog page that lists all the blog posts. It uses Suspense to show a loading state while the data is being fetched.
+export default function BlogPage() {
   return (
     <div className='py-12'>
       <div className='text-center mb-12'>
@@ -20,37 +19,68 @@ export default async function BlogPage() {
           Insights, thoughts and trends from our team!
         </p>
       </div>
+      <Suspense fallback={<SkeletonLoadingUI />}>
+        <LoadBlogList />
+      </Suspense>
+    </div>
+  );
+}
 
-      <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-        {data?.map((post) => (
-          <Card key={post._id} className='pt-0'>
-            <div className='relative h-48 w-full overflow-hidden'>
-              <Image
-                src='https://images.unsplash.com/photo-1771506364945-0b6566c6cd5f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                alt='image'
-                fill
-                className='rounded-t-lg'
-              />
-            </div>
-            <CardContent>
-              <Link href={'/blog/${post._id}'}>
-                <h1 className='text-2xl font-bold hover:text-primary'>
-                  {post.title}
-                </h1>
-              </Link>
-              <p className='text-muted-foreground line-clamp-3'>{post.body}</p>
-            </CardContent>
-            <CardFooter>
-              <Link
-                className={buttonVariants({ className: 'w-full' })}
-                href={'/blog/${post._id}'}
-              >
-                Read more
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+// This component fetches the list of blog posts and renders them. It is wrapped in a Suspense component to show a loading state while the data is being fetched.
+async function LoadBlogList() {
+  //suspense test
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const data = await fetchQuery(api.posts.getPosts);
+
+  return (
+    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      {data?.map((post) => (
+        <Card key={post._id} className='pt-0'>
+          <div className='relative h-48 w-full overflow-hidden'>
+            <Image
+              src='https://images.unsplash.com/photo-1771506364945-0b6566c6cd5f?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='image'
+              fill
+              className='rounded-t-lg'
+            />
+          </div>
+          <CardContent>
+            <Link href={'/blog/${post._id}'}>
+              <h1 className='text-2xl font-bold hover:text-primary'>
+                {post.title}
+              </h1>
+            </Link>
+            <p className='text-muted-foreground line-clamp-3'>{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              className={buttonVariants({ className: 'w-full' })}
+              href={'/blog/${post._id}'}
+            >
+              Read more
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+// This is a simple skeleton loading UI that mimics the structure of the blog list while the data is being fetched. You can customize it to better fit your design.
+function SkeletonLoadingUI() {
+  return (
+    <div className='grid gap-6 md:grid-cols-3 lg:grid-cols-3'>
+      {[...Array(3)].map((_, i) => (
+        <div key={i} className='flex flex-col space-y-3'>
+          <Skeleton className='h-48 w-full rounded-xl' />
+          <div className='space-y-2 flex flex-col'>
+            <Skeleton className='h-6 w-3/4' />
+            <Skeleton className='h-4 w-full' />
+            <Skeleton className='h-4 w-2/3' />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
